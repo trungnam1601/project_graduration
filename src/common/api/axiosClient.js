64a1a3c56bcf1c9f489e574751b55c13f63b6503
NamMT <trungnam16hd@gmail.com>
2023-08-08 16:49:55 +0700

    update url backend

--- src/common/api/axiosClient.js
+++ src/common/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://contabo.foxcode.site:8087';
-const BASE_URL = 'http://localhost:8087';
+// const BASE_URL = 'http://localhost:8087';
+const BASE_URL = 'http://nammt.io.vn:8087';
 
 const axiosClient = axios.create({
     baseURL: BASE_URL,
